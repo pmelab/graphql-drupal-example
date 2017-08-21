b2d87d525b51bf98d5faf058f3b0d5327ec129d1
Philipp Melab <philipp.melab@gmail.com>
2017-08-21 13:12:32 +0200

    Retrieve page titles from external urls.

---
+++ src/Plugin/GraphQL/Fields/ExampleRoute.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\graphql_example\Plugin\GraphQL\Fields;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Url;
+use Youshido\GraphQL\Execution\ResolveInfo;
+use Drupal\graphql_core\Plugin\GraphQL\Fields\Route;
+
+/**
+ * Retrieve a url object based on an internal path or external url.
+ *
+ * @GraphQLField(
+ *   id = "example_url_route",
+ *   name = "route",
+ *   type = "Url",
+ *   nullable = true,
+ *   arguments = {
+ *     "path" = "String"
+ *   },
+ *   weight = 1
+ * )
+ */
+class ExampleRoute extends Route {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolveValues($value, array $args, ResolveInfo $info) {
+    if (UrlHelper::isExternal($args['path'])) {
+      yield Url::fromUri($args['path']);
+    }
+    else {
+      foreach (parent::resolveValues($value, $args, $info) as $item) {
+        yield $item;
+      }
+    }
+  }
+
+}
--- src/Plugin/GraphQL/Fields/PageTitle.php
+++ src/Plugin/GraphQL/Fields/PageTitle.php
@@ -28,9 +28,19 @@ class PageTitle extends FieldPluginBase {
    */
   protected function resolveValues($value, array $args, ResolveInfo $info) {
     if ($value instanceof Url) {
-      if ($route = \Drupal::service('router.route_provider')->getRouteByName($value->getRouteName())) {
+      if ($value->isRouted() && $route = \Drupal::service('router.route_provider')->getRouteByName($value->getRouteName())) {
         $request = Request::create($value->toString());
         yield \Drupal::service('title_resolver')->getTitle($request, $route);
+      }
+      else {
+        $content = (string) \Drupal::httpClient()
+          ->get($value->toString())
+          ->getBody();
+        $doc = new \DOMDocument();
+        $doc->loadHTML($content);
+        $xpath = new \DOMXPath($doc);
+        $title = $xpath->query('//title')->item(0)->textContent;
+        yield (string) $title;
       }
     }
   }
